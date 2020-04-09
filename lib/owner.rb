8f286f691daf16766424a65c5dd8dfb4af7e6502
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-09 22:49:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,63 +2,86 @@ require 'pry'
 
 class Owner
   attr_reader :species, :name
-  attr_accessor :dog, :cat
-  
+
+
   @@all = Array.new
-  
+
   def initialize(name,species = 'human')
     @name= name
     @species = species
     @@all.push(self)
-    @dog =[]
-    @cat = []
+
+
   end
-  
+
   def say_species
    "I am a #{species}."
   end
-  
+
   def self.all
+
     @@all
   end
-  
+
   def self.count
     @@all.length
   end
-  
-  def self.reset_all 
+
+  def self.reset_all
     @@all = Array.new
   end
-  
-  def cat
-    @cat
-    
+
+  def cats
+    Cat.all.select do |cats|
+      cats.owner == self
+    end
   end
-  
-  def dog
-    @dog
+
+  def dogs
+    Dog.all.select do |dogs|
+      dogs.owner == self
+    end
   end
-  
-  def buy_cat(add_cat) 
-    new_cat = Cat.new(add_cat)
-    @cat.push(new_cat)
-    @cat
+
+  def buy_cat(add_cat)
+    new_cat = Cat.new(add_cat,self)
+    @@all.push(new_cat)
+    @@all
   end
-  
-   
-  
-   def buy_dog(add_dog) 
-    new_dog = Dog.new(add_dog)
-    @dog.push(new_dog)
-    @dog
+
+
+
+   def buy_dog(add_dog)
+    new_dog = Dog.new(add_dog,self)
+    @@all.push(new_dog)
+    @@all
   end
-  
+
   def walk_dogs
-    self.each{|dog| mood = 'happy'}
+
+    Dog.all.select do |dogs|
+      dogs.mood = 'happy'
+    end
   end
-  
+
   def feed_cats
-    self.each{|cat| mood = 'happy'}
+    Cat.all.select do |cats|
+      cats.mood = 'happy'
+    end
+  end
+
+  def sell_pets
+
+    new_array = cats + dogs
+    new_array.each do |pet|
+      pet.mood = 'nervous'
+      pet.owner = nil
+    end
+
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 
 end
