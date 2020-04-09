66c08377a048c18410030b18f99acd7f52528dc6
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-09 15:30:14 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  attr_accessor :dog, :cat
+  
+  @@all = Array.new
+  
+  def initialize(name,species = 'human')
+    @name= name
+    @species = species
+    @@all.push(self)
+    @dog =[]
+    @cat = []
+  end
+  
+  def say_species
+   "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all 
+    @@all = Array.new
+  end
+  
+  def cat
+    @cat
+    
+  end
+  
+  def dog
+    @dog
+  end
+  
+  def buy_cat(add_cat) 
+    new_cat = Cat.new(add_cat)
+    @cat.push(new_cat)
+    @cat
+  end
+  
+   
+  
+   def buy_dog(add_dog) 
+    new_dog = Dog.new(add_dog)
+    @dog.push(new_dog)
+    @dog
+  end
+  
+  def walk_dogs
+    self.each{|dog| mood = 'happy'}
+  end
+  
+  def feed_cats
+    self.each{|cat| mood = 'happy'}
+  end
+
 end
