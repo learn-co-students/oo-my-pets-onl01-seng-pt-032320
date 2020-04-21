ff928c487688a58960db114a7f5d632fb2179f47
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-04-21 03:25:08 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,8 @@
+require 'pry'
+
 class Owner
   attr_reader :name, :species
   @@all = []
-  @@count = 0
   
   def initialize(name)
     @name = name
@@ -11,7 +12,6 @@ class Owner
   
   def save
     @@all << self
-    @@count += 1
   end
   
   def say_species
@@ -23,13 +23,50 @@ class Owner
   end
   
   def self.count
-    @@count
+    @@all.count
   end
   
   def self.reset_all
-    @@count = 0 
+    @@all.clear
   end
   
   def cats 
+    Cat.all.select {|cat| cat.owner == self}
   end
+
+  def buy_cat(cat_for_sale)
+    Cat.new(cat_for_sale, self)
+  end
+
+  def feed_cats
+    Cat.all.each {|cat| cat.mood = "happy"}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_dog(dog_for_sale)
+    Dog.new(dog_for_sale, self)
+  end
+
+  def walk_dogs
+    Dog.all.each {|dog| dog.mood = "happy"}
+  end
+
+  def sell_pets
+    Dog.all.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+    Cat.all.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
