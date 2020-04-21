f1bf42c19e00340591ed4aafb89f7edb36a37f0a
Allan Farca <allan.farca@gmail.com>
2020-04-21 04:31:49 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+  
+  def say_species
+    return "I am a human."
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.count
+    @@all.size
+  end 
+  
+  def self.reset_all
+    @@all = []
+  end 
+  
+  def cats (number)
+  end 
+  
+  def dogs (number)
+  end 
+  
 end
