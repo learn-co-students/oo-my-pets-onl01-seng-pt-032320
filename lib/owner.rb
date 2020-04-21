77b5688f46bb8ba0e2018ac88e0a43e89b139c47
Evan Smith <professionalemail@zoho.com>
2020-04-21 02:29:46 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+  
+  def save
+    @@all << self
+    @@count += 1
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@count = 0 
+  end
+  
+  def cats 
+  end
 end
