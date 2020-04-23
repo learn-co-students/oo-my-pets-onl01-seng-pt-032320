ead90e8a40fab8ae08cc48bca5f5d0dde79a5bd6
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-23 05:02:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(cat_name, owner)
+    @name = cat_name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
