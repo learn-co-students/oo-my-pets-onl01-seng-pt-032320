4f3dcd5782005147c4a5bb5db6cb5613d2613d4f
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 04:49:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
+require "pry"
 class Owner
-  # code goes here
+ attr_accessor 
+ attr_reader :name, :species
+
+ 
+ @@all = []
+ @@counter = 0
+ 
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@counter += 1
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count 
+    @@counter
+  end
+ # binding.pry
+  def self.reset_all
+    @@all.clear
+    @@counter = 0
+  end
+  
+  def say_species
+    "I am a #{@species}."
+  end
 end
