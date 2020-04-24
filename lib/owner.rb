c84e4b14a7fc6630ab45548d72848119fa5c2d8d
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 16:57:00 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,18 @@
 require "pry"
 class Owner
- attr_accessor 
+ attr_accessor :cats, :dogs
  attr_reader :name, :species
 
  
  @@all = []
  @@counter = 0
  
+ 
   def initialize(name)
     @name = name
     @species = "human"
+    @dogs = []
+    @cats = []
     @@all << self
     @@counter += 1
   end
@@ -21,7 +24,7 @@ class Owner
   def self.count 
     @@counter
   end
- # binding.pry
+
   def self.reset_all
     @@all.clear
     @@counter = 0
@@ -30,4 +33,63 @@ class Owner
   def say_species
     "I am a #{@species}."
   end
+  
+  def cats
+   Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs
+   Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    cat_obj = Cat.new(name, self)
+    @cats << cat_obj
+  end
+  
+  def buy_dog(name)
+    dog_obj = Dog.new(name, self)
+    @dogs << dog_obj
+  end
+  #binding.pry
+  def walk_dogs
+      Dog.all.select do |dogs|
+      dogs.mood = "happy"
+      end
+  end
+  
+  def feed_cats
+    Cat.all.select do |cats|
+      cats.mood = "happy"
+    end
+  end
+  
+  def sell_pets
+
+    pets = cats + dogs
+    pets.each do |pet|
+      pet.mood = 'nervous'
+      pet.owner = nil
+    end
+
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
