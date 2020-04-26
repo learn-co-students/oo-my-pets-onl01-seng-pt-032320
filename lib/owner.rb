0e8b795774dc80b59e834e5ef3ea6f08e4a27d21
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-26 12:12:02 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,13 +51,16 @@ class Owner
   end 
   
   def sell_pets
-    
-    
-  end
-  
-  def 
-    
+  pets = self.dogs + self.cats  #selling all pets 
+  pets.each do |pet|
+   pet.owner = nil #remember owners are leaving home to nyc so pets are being given away to adoption/hound and will no longer have owners therefore we use nil 
+   pet.mood = "nervous"
+
+    end 
   end 
   
-  
+  def list_pets
+  "I have 2 dog(s), and 2 cat(s)."
+  end 
+
 end
