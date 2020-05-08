eff93e96739ff3bc586ce701d420e583f1754e03
Malcom Fetus <sora.vista@gmail.com>
2020-05-08 01:09:58 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+@@all = [] #set an empty array so instances can be inserted there
+
+def initialize(name)
+  @name = name
+  @species = "human"
+  @@all << self #all instances of the class object (self) will be added to the @@all which has an empty array and now carries the self instances
+  #binding.pry
+end
+
+def say_species
+  @species = "I am a human."
+  @species
+
+end
+
+def self.all
+@@all
+#binding.pry
+end
+
+def self.count
+@@all.count
+end
+
+def self.reset_all
+@@all.clear
+end
+
+
 end
