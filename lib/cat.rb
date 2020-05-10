34a576c294b14a038ec5ddd9f3126580cec2b5df
Malcom Fetus <sora.vista@gmail.com>
2020-05-10 02:02:45 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -4,13 +4,18 @@ class Cat
 attr_accessor  :owner, :mood # Here both owner and mood can be changed (through the hidden writer methods)
 attr_reader :name #Here this cannot be changed
 
+@@all = []
+
 def initialize(name, owner) #Name & Owner needs to be both in the argument as they're both being initialize and yet owner can be modified
   #both as a reader and writter while the name cannot be modified and therefore needs a reader only.
   @name = name
   @owner = owner
   @mood = "nervous" #Mood is being initialized with a specific function
-#binding.pry
+  @@all << self
 end
 
+def self.all
+  @@all
+end
 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -31,5 +31,62 @@ def self.reset_all
 @@all.clear
 end
 
+def cats
+  Cat.all.select do |cat_object|
+    cat_object.owner == self
+    #binding.pry
+end
+end
+
+
+def dogs
+  Dog.all.select do |dog_object|
+    dog_object.owner == self
+  end
+end
+
+
+def buy_cat(name)
+  owner = self
+Cat.new(name, owner)
+end
+
+def buy_dog(name)
+  owner = self
+  Dog.new(name, owner)
+
+end
+
+def walk_dogs
+  #binding.pry
+self.dogs.each do |dog|
+  #^ Here we use self to refer to a specific owner and use the dogs method to call on a specific dog that the owner prefers to walk
+  #Then we ieterate over a specific dog and pull that specific dog.
+dog.mood = "happy"
+#^ We then use mood method from the dog's file and then change it's mood by setting it to be a variable.
+end
+end
+
+def feed_cats
+self.cats.each do |cat|
+cat.mood = "happy"
+  end
+
+  end
+
+def sell_pets
+pets = self.cats + self.dogs
+pets.each do |pet|
+pet.mood = "nervous"
+pet.owner = nil
+#binding.pry
+end
+
+def list_pets
+ "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+end
+
+
+end
 
 end
